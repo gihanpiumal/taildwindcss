b36937a318ccbcb6c30009345579ebb32037ed3d
gihanpiumal <gihanpiumal12345@gmail.com>
2024-11-30 20:18:30 +0530

    setup tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <main className="relative">
+      NAV
+      <section className="">
+        Hero
+      </section>
+    </main>
+  );
+};
+
+export default App;
